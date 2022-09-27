9bbba2823198cebd3ef1bcb58535ad402845ee64
montescar19dev <carlosmontes19.dev@gmail.com>
2022-09-26 22:40:52 -0400

    ultimos detalles

--- src/components/UsersList.jsx
+++ src/components/UsersList.jsx
@@ -1,4 +1,5 @@
 import React from "react";
+import avatar from "../assets/avatar-profile.jpg"
 
 const UsersList = ({ users, deleteUser, selectUser }) => {
   return (
@@ -6,18 +7,23 @@ const UsersList = ({ users, deleteUser, selectUser }) => {
       <h1>Users List</h1>
       <ul className="users-list">
         {users.map((user) => (
-          <li key={user.id}>
+          <li className="usersCards" key={user.id}>
+            <div>
+              <img className="photo" src={avatar} alt="" />
+            </div>
             <h3>{user.first_name} {user.last_name}</h3>
             <div>
+              {<i class="fa-solid fa-envelope"></i>}{" "}
               <b>Email: </b>
               {user.email}
             </div>
             <div>
+              {<i class="fa-solid fa-cake-candles"></i>}{" "}
               <b>Birthday: </b>
               {user.birthday}
             </div>
-            <button onClick={() => deleteUser(user.id)}>Delete User</button>
-            <button onClick={() => selectUser(user)}>Update info</button>
+            <button className="UserButtons" onClick={() => deleteUser(user.id)}>Delete User</button>
+            <button className="UserButtons" onClick={() => selectUser(user)}>Update info</button>
           </li>
         ))}
       </ul>
